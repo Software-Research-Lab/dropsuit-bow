b57398ffa4c065c2a2a1122ce678c03955418df9
Lado Oniani <ladooniani@gmail.com>
2023-02-25 03:09:22 +0400

    Updated test for word count based on corpus presence.

--- test/index.test.js
+++ test/index.test.js
@@ -13,7 +13,7 @@ let inputArray = [
 ];
 
 let ClassicBOW =
-  "Classic BOW: Counts the frequency of words in a given input sentence: [1, 2, 1, 1, 2, 1, 1].";
+  "Bag of Words: Counts the frequency of words in a given input sentence: [1, 2, 1, 1, 2, 1, 1].";
 
 let expected_output_bow_bowb1 = {
   john: 1,
@@ -25,21 +25,16 @@ let expected_output_bow_bowb1 = {
   too: 1,
 };
 
-let BOWwithVocabulary =
-  "BOW with Vocabulary: Counts the frequency of words in a given input sentence against a corpus of input sentences: [1, 2, 1, 1, 2, 1, 1, 0, 0, 0]";
+let CorpusBasedFrequencyCount =
+  "Corpus Based Frequency Count: Counts the frequency of words in a given input sentence against a corpus of input sentences: [0, 2, 1, 1]";
 
-let expected_output_bow_bowb2 = {
-  john: 1,
-  likes: 2,
-  to: 1,
-  watch: 1,
-  movies: 2,
-  mary: 1,
-  too: 1,
-};
+let expected_val1 = [2, 1, 1, 1, 1];
+let expected_bow1 = { a: 2, e: 1, b: 1, c: 1, u: 1 };
+let expected_val2 = [0, 2, 1, 1];
+let expected_bow2 = { a: 2, b: 1, c: 1 };
 
-let CorpusBOW =
-  "Corpus BOW: Counts the frequency of words in a corpus of input sentences: [1, 3, 2, 2, 2, 2, 1, 1, 1, 1].";
+let Corpus =
+  "Corpus: Counts the frequency of words in a corpus of input sentences: [1, 3, 2, 2, 2, 2, 1, 1, 1, 1].";
 
 let expected_output_bow_bowb3 = {
   john: 1,
@@ -54,14 +49,14 @@ let expected_output_bow_bowb3 = {
   games: 1,
 };
 
-let BOWComparison =
-  "BOW Comparison: Counts the frequency of words in one input sentence against another input sentence (optional) [0, 1, 1, 1, 0, 1, 0] or [1, 0, 2, 1, 1, 0, 0].";
+let Comparison =
+  "Comparison: Counts the frequency of words in one input sentence against another input sentence (optional) [0, 1, 1, 1, 0, 1, 0] or [1, 0, 2, 1, 1, 0, 0].";
 
 let expected_output_bow_bowb4 = { mary: 1, likes: 1, to: 1, watch: 1 };
 let expected_output_bow_bowb5 = { likes: 2, to: 1, watch: 1, mary: 1 };
 
-let CorpusComparisonBOW =
-  "Corpus Comparison BOW: Counts the frequency of words in a corpus of input sentences against one input sentence (optional) [1, 3, 2, 2, 2, 2, 1] or [2, 1, 3, 2, 2, 1, 1].";
+let CorpusComparison =
+  "Corpus Comparison: Counts the frequency of words in a corpus of input sentences against one input sentence (optional) [1, 3, 2, 2, 2, 2, 1] or [2, 1, 3, 2, 2, 1, 1].";
 
 let expected_output_bow_bowb6 = {
   john: 1,
@@ -136,14 +131,39 @@ describe("dropsuit-bow", () => {
       assert.deepEqual(bow_bowb_output1, expected_output_bow_bowb1);
     });
 
-    it(BOWwithVocabulary, () => {
-      let dsbow2 = new dropsuit_bow(inputArray, false);
-      let bow_bowb_output2 = dsbow2.bow(inputArray[0]).bow();
-      console.log("\n\nOutput bow():", bow_bowb_output2, "\n\n");
-      assert.deepEqual(bow_bowb_output2, expected_output_bow_bowb2);
+    /// Count word frequency in a sentence based on the corpus of documents,
+    // ignoring non-persistent words and returning zero for unknown words.
+    it(CorpusBasedFrequencyCount, () => {
+      let inputs = "a e b c u a";
+      let vocabularyCorpus = ["x a b c", "a b c x", "c a x b x"];
+      // BOW normal
+      let dsbow1 = new dropsuit_bow(null, false);
+      let val1 = dsbow1.bow(inputs).value();
+      let bow1 = dsbow1.bow(inputs).bow();
+      // BOW with vocabulary
+      let dsbow2 = new dropsuit_bow(vocabularyCorpus, false);
+      let val2 = dsbow2.bow(inputs).value();
+      let bow2 = dsbow2.bow(inputs).bow();
+      //#region console log
+      console.log(
+        "\n\nOutput BOW-Normal bow().value():",
+        val1,
+        "\nbow():",
+        bow1,
+        "\nOutput BOW-Vocabulary bow().value():",
+        val2,
+        "\nbow():",
+        bow2
+      );
+      //#endregion
+
+      assert.deepEqual(val1, expected_val1);
+      assert.deepEqual(bow1, expected_bow1);
+      assert.deepEqual(val2, expected_val2);
+      assert.deepEqual(bow2, expected_bow2);
     });
 
-    it(CorpusBOW, () => {
+    it(Corpus, () => {
       //#region equal input-output
       /**
        * Same result:
@@ -161,7 +181,7 @@ describe("dropsuit-bow", () => {
       assert.deepEqual(bow_bowb_output3, expected_output_bow_bowb3);
     });
 
-    it(BOWComparison, () => {
+    it(Comparison, () => {
       let dsbow4 = new dropsuit_bow(inputArray[0], false);
       let bow_bowb_output4 = dsbow4.bow(inputArray[1]).bow();
       console.log("\n\nOutput bow():", bow_bowb_output4, "\n\n");
@@ -173,7 +193,7 @@ describe("dropsuit-bow", () => {
       assert.deepEqual(bow_bowb_output5, expected_output_bow_bowb5);
     });
 
-    it(CorpusComparisonBOW, () => {
+    it(CorpusComparison, () => {
       let dsbow6 = new dropsuit_bow(inputArray[0], false);
       let bow_bowb_output6 = dsbow6.bow(inputArray).bow();
       console.log("\n\nOutput bow():", bow_bowb_output6, "\n\n");
